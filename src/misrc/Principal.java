2e54764ac2dbd6d1cd24eb84a77e54e6eda184f6
DATASAGASTEGUI <datasagamadrid@gmail.com>
2024-07-10 11:17:33 +0200

    FOTO 2

---
+++ src/misrc/Principal.java
@@ -0,0 +1,20 @@
+package misrc;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        /*
+        dfad
+                fasd
+                fasd
+                        fa
+        
+        
+        
+                        dsfa*/
+                                
+    }
+    
+}
